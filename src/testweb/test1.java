6b8fe2ed1f79b6e4ceb74f33a600d4214651fe72
yanghaibin <yanghaibin@LDS-Z-C5537.LEEDARSON.LOCAL>
2020-03-26 10:50:30 +0800

    web项目测试

---
+++ src/testweb/test1.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package testweb;
+
+/**
+ *
+ *
+ * @auhor yanghaibin
+ * @Create_Date: 2019年7月23日下午6:43:55
+ * @last_modify by yanghaibin at 2019年7月23日下午6:43:55 <br>
+ * @Why_and_What_is_modified:
+ */
+public class test1{
+	private String prop1;
+    private Integer prop2;
+    private Double  prop3;
+	public String getProp1() {
+		return prop1;
+	}
+	public void setProp1(String prop1) {
+		this.prop1 = prop1;
+	}
+	public Integer getProp2() {
+		return prop2;
+	}
+	public void setProp2(Integer prop2) {
+		this.prop2 = prop2;
+	}
+	public Double getProp3() {
+		return prop3;
+	}
+	public void setProp3(Double prop3) {
+		this.prop3 = prop3;
+	}
+    
+   
+}
